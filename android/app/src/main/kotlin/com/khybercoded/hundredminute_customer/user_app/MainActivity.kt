dac36b732a23f8a132ede0e160c6f2347a4de88a
Modassir Habib <modassirhabib9@gmail.com>
2021-12-16 20:56:23 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/khybercoded/hundredminute_customer/user_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.khybercoded.hundredminute_customer.user_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/repository/product_repo.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:user_app/data/datasource/remote/dio/dio_client.dart';
+import 'package:user_app/data/datasource/remote/exception/api_error_handler.dart';
+import 'package:user_app/data/model/response/base/api_response.dart';
+import 'package:user_app/utill/app_constants.dart';
+
+class ProductRepo {
+  final DioClient dioClient;
+
+  ProductRepo({@required this.dioClient});
+
+  Future<ApiResponse> getLatestProductList(String offset) async {
+    print("Ok 11");
+    try {
+      print("Ok 12");
+      final response =
+          await dioClient.get(AppConstants.LATEST_PRODUCTS_URI + offset);
+      return ApiResponse.withSuccess(response);
+    } catch (e) {
+      print("Ok 13");
+      return ApiResponse.withError(ApiErrorHandler.getMessage(e));
+    }
+  }
+
+  Future<ApiResponse> getSellerProductList(
+      String sellerId, String offset) async {
+    try {
+      final response = await dioClient.get(AppConstants.SELLER_PRODUCT_URI +
+          sellerId +
+          '/products?limit=10&&offset=' +
+          offset);
+      return ApiResponse.withSuccess(response);
+    } catch (e) {
+      return ApiResponse.withError(ApiErrorHandler.getMessage(e));
+    }
+  }
+
+  Future<ApiResponse> getBrandOrCategoryProductList(
+      bool isBrand, String id) async {
+    try {
+      String uri;
+      if (isBrand) {
+        uri = '${AppConstants.BRAND_PRODUCT_URI}$id';
+      } else {
+        uri = '${AppConstants.CATEGORY_PRODUCT_URI}$id';
+      }
+      final response = await dioClient.get(uri);
+      return ApiResponse.withSuccess(response);
+    } catch (e) {
+      return ApiResponse.withError(ApiErrorHandler.getMessage(e));
+    }
+  }
+
+  Future<ApiResponse> getRelatedProductList(String id) async {
+    try {
+      final response =
+          await dioClient.get(AppConstants.RELATED_PRODUCT_URI + id);
+      return ApiResponse.withSuccess(response);
+    } catch (e) {
+      return ApiResponse.withError(ApiErrorHandler.getMessage(e));
+    }
+  }
+}
